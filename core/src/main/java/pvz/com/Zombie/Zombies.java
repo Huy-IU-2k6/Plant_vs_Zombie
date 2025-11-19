c37ef2062a500e110337932ab36dd5d0232818c8
justa-newbie <ITITWE23026@student.hcmiu.edu.vn>
2025-11-19 13:15:14 +0700

    add Zombies class

---
+++ core/src/main/java/pvz/com/Zombie/Zombies.java
@@ -0,0 +1,70 @@
+package pvz.com.zombies;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public class Zombies extends Actor {
+
+    protected int health ;
+    protected float speed;
+    protected static boolean gameOver = false;
+    protected static int zombieCount = 0;
+
+    protected Sound comingZombieSound;
+
+    public Zombies() {
+
+        
+        comingZombieSound = Gdx.audio.newSound(Gdx.files.internal("coming_zombie.wav"));
+
+        
+        if (zombieCount == 0) {
+            comingZombieSound.play();
+        }
+
+        zombieCount++;
+    }
+
+    public void update(float delta) {
+        
+        if (!isTouchingPlant()) {
+            moveBy(-speed * delta, 0);
+        }
+
+        checkGameOver();
+    }
+
+    
+    public void takeDamage(int dmg) {
+        health -= dmg;
+
+        if (health <= 0) {
+            die();
+        }
+    }
+    protected void die() {
+        
+
+        
+        Stage stage = getStage();
+        if (stage != null) {
+            stage.getRoot().removeActor(this);
+        }
+    }
+
+    
+    protected void checkGameOver() {
+        if (getX() < 260) {
+            gameOver = true;
+            
+        }
+    }
+
+    
+    protected boolean isTouchingPlant() {
+        
+        return false;
+    }
+}
