e79ba1605259bf0ea2802b7f024b8d61a5a2563d
justa-newbie <ITITWE23026@student.hcmiu.edu.vn>
2025-11-24 21:44:45 +0700

    Add files via upload

--- core/src/main/java/pvz/com/Zombies/Zombies.java
+++ core/src/main/java/pvz/com/Zombies/Zombies.java
@@ -1,84 +1,69 @@
-package pvz.com.Zombies;
-
+package pvz.com.zombies;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.math.Rectangle; // <--- thêm dòng này
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class Zombies extends Actor {
 
-    // ----- STATIC STATE -----
+    protected int health ;
+    protected float speed;
     protected static boolean gameOver = false;
     protected static int zombieCount = 0;
-    private static final Sound comingZombieSound = Gdx.audio
-            .newSound(Gdx.files.internal("assets/sounds/zombies_are_coming.wav"));
 
-    // ----- INSTANCE STATE -----
-    protected int health;
-    protected float speed;
-
-    // đánh dấu đã chết chưa (để không xử lý lại)
-    protected boolean dead = false;
+    protected Sound comingZombieSound;
 
     public Zombies() {
-        // Chỉ phát sound khi con zombie đầu tiên xuất hiện
+
+        
+        comingZombieSound = Gdx.audio.newSound(Gdx.files.internal("coming_zombie.wav"));
+
+        
         if (zombieCount == 0) {
             comingZombieSound.play();
         }
+
         zombieCount++;
     }
 
     public void update(float delta) {
+        
         if (!isTouchingPlant()) {
             moveBy(-speed * delta, 0);
         }
+
         checkGameOver();
     }
 
+    
     public void takeDamage(int dmg) {
-        if (dead)
-            return;
         health -= dmg;
+
         if (health <= 0) {
             die();
         }
     }
-
-    // Cho mấy thằng khác gọi thẳng
-    public void kill() {
-        if (!dead) {
-            die();
-        }
-    }
-
     protected void die() {
-        dead = true;
+        
 
-        // giảm số lượng zombie đang sống (phòng dư thôi)
-        if (zombieCount > 0) {
-            zombieCount--;
+        
+        Stage stage = getStage();
+        if (stage != null) {
+            stage.getRoot().removeActor(this);
         }
-
-        // Actor đã có sẵn remove() để tự xoá khỏi Stage
-        remove();
     }
 
+    
     protected void checkGameOver() {
-        if (!dead && getX() < 260) {
+        if (getX() < 260) {
             gameOver = true;
+            
         }
     }
 
+    
     protected boolean isTouchingPlant() {
+        
         return false;
-    }
-
-    // Cho lawn mower / va chạm khác dùng
-    public Rectangle getBounds() {
-        return new Rectangle(getX(), getY(), getWidth(), getHeight());
-    }
-
-    public boolean isDead() {
-        return dead;
     }
 }
