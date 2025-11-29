aff82fb820ad0f15ed1373a519378a8709c44c90
Huy-IU-2k6 <ITCSIU24034@student.hcmiu.edu.vn>
2025-11-29 09:32:29 +0700

    resolve conflict

--- core/src/main/java/pvz/com/entities/projectiles/PeaProjectile.java
+++ core/src/main/java/pvz/com/entities/projectiles/PeaProjectile.java
@@ -8,35 +8,6 @@ public class PeaProjectile extends Entity {
     public PeaProjectile(float x, float y, int damage) {
         super();
 
-<<<<<<< HEAD
-        // 1. Vị trí & Kích thước (Đạn nhỏ, khoảng 20x20)
-        this.addComponent(new PositionComponent(startX, startY));
-        this.addComponent(new SizeComponent(20, 20));
-        this.addComponent(new BoundsComponent(startX, startY, 20, 20));
-
-        // 2. Hình ảnh
-        this.addComponent(new SpriteComponent("assets/images/Projectiles/pea.png"));
-
-        // 3. Chuyển động (QUAN TRỌNG)
-        // Đạn bay thẳng sang phải với tốc độ 400
-        this.addComponent(new MovementComponent(400f, 0f));
-
-        // 4. Sát thương
-        // Viên đạn mang thông tin sát thương để khi va chạm Zombie sẽ trừ máu
-        // Ta tái sử dụng PlantAttackComponent nhưng chỉ dùng trường 'damage'
-        // Hoặc tạo một component mới đơn giản hơn là DamageComponent (tùy bạn, ở đây tận dụng cái cũ cho lẹ)
-        this.addComponent(new PlantAttackComponent(
-            damage, 
-            0, null, PlantDamageType.NORMAL, 0 // Mấy tham số sau không quan trọng với đạn
-        ));
-
-        // 5. Phe phái (PROJECTILE)
-        // CollisionSystem sẽ check: Nếu PROJECTILE va chạm ZOMBIE -> Gây damage và Xóa đạn
-        this.addComponent(new TeamComponent(Team.NONE)); // Hoặc Team.PLANT tùy logic va chạm của bạn
-        
-        // 6. Tự hủy khi bay ra khỏi màn hình
-        // Logic này sẽ được xử lý bởi CleanupSystem kiểm tra tọa độ x > màn hình
-=======
         this.addComponent(new PositionComponent(x, y));
         this.addComponent(new MovementComponent(200f, 0f));
         this.addComponent(new SpriteComponent("assets/images/Plants/peabullet.png"));
@@ -48,6 +19,5 @@ public class PeaProjectile extends Entity {
 
         // Tag để ProjectileCollisionSystem nhận diện đây là đạn
         this.addComponent(new ProjectileTagComponent());
->>>>>>> 2ba7b6f81dcc998e313ca56f50281196a902a14d
     }
 }
